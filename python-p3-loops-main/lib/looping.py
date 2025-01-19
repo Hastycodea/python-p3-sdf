07ec382318e7e7826d5f011252ca9eb1cc840dec
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-01-19 13:22:30 +0300

    loops

---
+++ python-p3-loops-main/lib/looping.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def happy_new_year():
+    i = 10
+    while(i >= 0):
+        if(i == 0):
+            print("Happy New Year!")
+        else:
+            print(i)
+        i -= 1
+
+def square_integers(int_list):
+    int_list = [num * num for num in int_list]
+    return int_list
+
+def fizzbuzz():
+    for i in range(1,101):
+        if(i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif(i%3 == 0):
+            print("Fizz")
+        elif(i%5 == 0):
+            print("Buzz")
+        else:
+            print(i)
