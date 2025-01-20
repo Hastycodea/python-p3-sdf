0c55d49ff2153ac3928a29e365388f62fbc4cc51
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-01-20 08:45:39 +0300

    cuisine

--- python-p3-data-structures-lab-main/lib/data_structures.py
+++ python-p3-data-structures-lab-main/lib/data_structures.py
@@ -30,12 +30,16 @@ def print_spicy_foods(spicy_foods):
         cuisine = spicy_food["cuisine"]
         heat_level = spicy_food["heat_level"] 
 
-        print(f"{name} ({cuisine}) | Heat Level: U+1F336 * {heat_level}")
+        print(f"{name} ({cuisine}) | Heat Level: U+1F336*{heat_level}")
 
-print_spicy_foods(spicy_foods)
+# print_spicy_foods(spicy_foods)
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    spicy_food = [spicy_food for spicy_food in spicy_foods if spicy_food["cuisine"] == cuisine]
+    return spicy_food[0]
+
+print(get_spicy_food_by_cuisine(spicy_foods, "American"))
 
 def print_spiciest_foods(spicy_foods):
     pass
