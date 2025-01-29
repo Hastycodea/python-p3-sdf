6fb43bda4cdf47dc71e1a8e9172bff8f76264655
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-01-29 08:33:16 +0300

    anagramUpdated

--- python-p3-anagram-detector-lab-main/lib/anagram.py
+++ python-p3-anagram-detector-lab-main/lib/anagram.py
@@ -1,5 +1,16 @@
 # your code goes here!
 
-def class Anagram:
-    pass
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        result = []
+
+        for w in words:
+            if sorted([letter for letter in w]) == sorted([l for l in self.word]): 
+                result.append(w)
+                
+        return result
+            
 
