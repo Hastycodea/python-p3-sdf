46d0908c4eb451416e3b22bbeb780ea10d86eec6
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-02-02 20:13:40 +0300

    inheritancePY

---
+++ python-p3-inheritance-lab-main/lib/student.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from user import User
+
+class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+    
+    def learn(self, stack):
+        self.knowledge.append(stack)
+
